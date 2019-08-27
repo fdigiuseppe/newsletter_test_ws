d4e0ad37b690b11110b9ab098c23b9a2759ceed7
Fabio Di Giuseppe <fabio.digiuseppe84@gmail.com>
2019-08-27 13:29:16 +0200

    aggiunto controllo , tra 2 indirizzi mail

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-$(document).ready(function () {
-	$("#nwl_form").submit(function (event) {
-		//alert("Handler for .submit() called.");
-		event.preventDefault();
+$(document).ready(function() {
 
-		$.ajax({
-			type: 'post',
-			url: 'handler.php',
-			data: $('#nwl_form').serialize(),
-			dataType: 'json',
-			success: function (data) {
-				//alert('form was submitted');
-				console.log(data);
-				if (data.status) {
-					$('#nwl_form').trigger("reset");
-					$("#success_message").fadeIn();
-					setTimeout(function(){
-						$("#success_message").fadeOut();
-					}, 2500);
-				}
-				else {
-					$("#error_message").fadeIn();
-				}
-			}
-		});
-		
-	});
+    $("#email").keyup(function(event) {
+        //console.log(event.which);
+				$(this).val($(this).val().replace(/[,]/g, ""));
+		}); 
+
+    $("#nwl_form").submit(function(event) {
+        //alert("Handler for .submit() called.");
+        event.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: 'handler.php',
+            data: $('#nwl_form').serialize(),
+            dataType: 'json',
+            success: function(data) {
+                //alert('form was submitted');
+                console.log(data);
+                if (data.status) {
+                    $('#nwl_form').trigger("reset");
+                    $("#success_message").fadeIn();
+                    setTimeout(function() {
+                        $("#success_message").fadeOut();
+                    }, 2500);
+                } else {
+                    $("#error_message").fadeIn();
+                }
+            }
+        });
+
+    });
 });
